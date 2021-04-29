e53917f231e8836e096102ba798dd5e5f98e2de8
unknown <sjssarvan05@gmail.com>
2021-04-29 15:01:21 +0530

    chat application

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { UserContext } from './UserContext';
+import Chat from './components/chat/Chat';
+import Home from './components/home/Home';
+import Navbar from './components/layout/Navbar';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
+function App() {
+  const [user, setUser] = useState(null)
+  useEffect(()=>{
+    const verifyuser=async()=>{
+      try{
+        const res = await fetch('http://localhost:5000/verifyuser', {
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/json' }
+      });
+      const data=await res.json()
+      setUser(data)
+      }catch(error){
+        console.log(error);
+      }
+    }
+    verifyuser();
+  },[]);
+  return (
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{ user, setUser }}>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/chat/:room_id/:room_name" component={Chat} />
+
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
